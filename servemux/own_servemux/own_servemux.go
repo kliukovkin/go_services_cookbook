d57acd1abcb29378df6ec130f056a7af5cdacdc0
ACSAIS <kliukovkin@gmail.com>
2022-05-20 08:20:05 +0400

    implementing own servemux

---
+++ servemux/own_servemux/own_servemux.go
@@ -0,0 +1,19 @@
+package handle_func
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+Same function signature as for HandlerServeHTTP, just a syntax sugar
+*/
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ping %s\n", r.URL.Query().Get("name"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", mux)
+}
