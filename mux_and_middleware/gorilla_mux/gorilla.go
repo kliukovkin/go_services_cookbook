0c092e2f72e2f19ecbd66fe366e91e28d5d7cf69
Kliukovkin <kliukovkin@gmail.com>
2022-07-13 16:01:44 +0300

    gorilla update

--- mux_and_middleware/gorilla_mux/gorilla.go
+++ mux_and_middleware/gorilla_mux/gorilla.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	 "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("test", handler).Methods("GET")
-	mux.
+	r.HandleFunc("/test", handler).Methods("GET")
+	http.ListenAndServe(":8000", r)
 }
